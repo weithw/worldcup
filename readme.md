7ee32a9c0f1eb4e4dc48413365241769f56156fc
weithw <llghwll@qq.com>
2014-06-20 23:46:26 +0800

    change the webroot

--- readme.md
+++
@@ -1,1 +0,0 @@
-**后台http://127.0.0.1:8083/index.php?g=admin&m=public&a=login**
--- webroot/index.php
+++ webroot/index.php
@@ -8,10 +8,12 @@
  * @version    1.0
  * @link       http://www.talkpiece.com
  */
+define('DIR_SECURE_FILENAME', 'default.html');
+define('DIR_SECURE_CONTENT', 'deney Access!');
 define('APP_DEBUG', true);
 define('APP_NAME', 'App');
-define('APP_PATH', './App/');
+define('APP_PATH', '../App/');
 if (!file_exists(APP_PATH .'Runtime/Install/install.lock')) {
 	$_GET['m'] = 'install';
 }
-require './ThinkPHP/bootstrap.php';
+require '../ThinkPHP/bootstrap.php';
